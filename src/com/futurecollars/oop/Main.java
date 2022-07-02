46bb5428494e730729eb74e1eab739a577bc44b2
Radek Borowski <radekborowski89@gmail.com>
2022-07-02 12:42:33 +0200

    OOP task1

---
+++ src/com/futurecollars/oop/Main.java
@@ -0,0 +1,2 @@
+package com.futurecollars.oop;public class Main {
+}
---
+++ src/com/futurecollars/oop/Movie.java
@@ -0,0 +1,47 @@
+package com.futurecollars;
+
+public class Movie {
+    private String movieTittle;
+    private String movieDirector;
+    private int movieDurationInMinutes;
+    private int yearOfProcudtion;
+
+    public String getMovieTittle() {
+        return movieTittle;
+    }
+
+    public void setMovieTittle(String movieTittle) {
+        this.movieTittle = movieTittle;
+    }
+
+    public String getMovieDirector() {
+        return movieDirector;
+    }
+
+    public void setMovieDirector(String movieDirector) {
+        this.movieDirector = movieDirector;
+    }
+
+    public int getMovieDurationInMinutes() {
+        return movieDurationInMinutes;
+    }
+
+    public void setMovieDurationInMinutes(int movieDurationInMinutes) {
+        this.movieDurationInMinutes = movieDurationInMinutes;
+    }
+
+    public int getYearOfProcudtion() {
+        return yearOfProcudtion;
+    }
+
+    public void setYearOfProcudtion(int yearOfProcudtion) {
+        this.yearOfProcudtion = yearOfProcudtion;
+    }
+
+    public Movie(String movieTittle, String movieDirector, int movieDurationInMinutes, int yearOfProcudtion) {
+        this.movieTittle = movieTittle;
+        this.movieDirector = movieDirector;
+        this.movieDurationInMinutes = movieDurationInMinutes;
+        this.yearOfProcudtion = yearOfProcudtion;
+    }
+    }
