3d97971c5e843b4178c0f1257dfa5a6460908fc6
Radek Borowski <r.borowski@vp.pl>
2022-07-19 18:12:01 +0200

    MOVIE

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,2 +1,33 @@
-package main.java;public class Movie {
+package main.java;
+
+public class Movie {
+
+    private final String title;
+    private final String director;
+    private final int durationInMinutes;
+    private final int yearOfProduction;
+
+    public Movie(String title, String director, int durationInMinutes, int yearOfProduction) {
+        this.title = title;
+        this.director = director;
+        this.durationInMinutes = durationInMinutes;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
 }
+
