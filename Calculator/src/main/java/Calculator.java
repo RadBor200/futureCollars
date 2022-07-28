a4b3c025c6324c065c61ebac5bf6452a5c531398
RadBor200 <107992044+RadBor200@users.noreply.github.com>
2022-07-28 14:25:33 +0200

    Update Calculator.java

--- Calculator/src/main/java/Calculator.java
+++ Calculator/src/main/java/Calculator.java
@@ -1,2 +1,28 @@
 package PACKAGE_NAME;public class Calculator {
+  
+   public static void main(String[] args) {
+
+        System.out.println(add(6, 7));
+        System.out.println(subtract(108, 9));
+        System.out.println(multiply(5, 7));
+        System.out.println(divide(100, 2));
+    }
+
+    public static int add(int a, int b) {
+
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
 }
+
