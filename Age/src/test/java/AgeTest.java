67716dc07c199164f0bba7abd4b701f610bf8a71
RadBor200 <107992044+RadBor200@users.noreply.github.com>
2022-08-08 10:56:51 +0200

    Update AgeTest.java

--- Age/src/test/java/AgeTest.java
+++ Age/src/test/java/AgeTest.java
@@ -4,29 +4,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AgeTest {
-
-        @Test
-        void shouldBeOfAge() {
-
-            int age = 18;
-
-            boolean result = Age.isOfAge(18);
-            Assertions.assertEquals(result, true);
-        }
-    @Test
-    void shouldBeOfAge2() {
-
-        int age = 20;
-
-        boolean result = Age.isOfAge(20);
-        Assertions.assertEquals(result, true);
-    }
-        @Test
-        void shouldNotBeOfAge() {
-
-            int age = 13;
-
-            boolean result = Age.isOfAge(13);
-            Assertions.assertEquals(result, false);
-        }
-    }
+@Test 
+void shouldBeOfAgeWhenAgeIs18() {
+int age = 18;
+boolean result = Age.isOfAge(18);
+Assertions.assertEquals(result, true);
+}
+@Test
+void shouldBeOfAgeWhenAgeIsMoreThan18() {
+int age = 20;
+boolean result = Age.isOfAge(20);
+Assertions.assertEquals(result, true);
+}
+@Test
+void shouldNotBeOfAge() {
+int age = 13;
+boolean result = Age.isOfAge(13);
+Assertions.assertEquals(result, false);
+}
+}
