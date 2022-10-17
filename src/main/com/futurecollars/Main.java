659f937147c2be12bb409028fe997265708f1d72
Radek Borowski <r.borowski@vp.pl>
2022-10-17 12:50:06 +0200

    Lesson 2 task3 : Calculator and Calculator test

--- src/main/com/futurecollars/Main.java
+++ src/main/com/futurecollars/Main.java
@@ -2,26 +2,6 @@ package com.futurecollars;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(add(6, 7));
-        System.out.println(subtract(108, 9));
-        System.out.println(multiply(5, 7));
-        System.out.println(divide(100, 2));
-    }
-    public static int add(int a, int b) {
-
-        int addResult = a + b;
-        return addResult;
-    }
-    public static int subtract(int a, int b) {
-        int subtractResult = a - b;
-        return subtractResult;
-    }
-    public static int multiply(int a, int b) {
-        int muliplyResult = a * b;
-        return muliplyResult;
-    }
-    public static int divide(int a, int b){
-        int divideResult = a / b;
-        return divideResult;
+        System.out.println("Hello World I'm on GitHub");
     }
 }
