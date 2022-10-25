19fd713a46ebac130be492f31886d905770ee0d2
Radek Borowski <r.borowski@vp.pl>
2022-10-25 12:08:19 +0200

    Lesson 6 task 1 data.txt reader

---
+++ src/main/java/com/futurecollars/task1/App.java
@@ -0,0 +1,9 @@
+package com.futurecollars.task1;
+
+public class App {
+    public static void main(String[] args) {
+      ReaderApp readerApp = new ReaderApp();
+      readerApp.showTextInConsoleFromFile();
+    }
+  }
+
