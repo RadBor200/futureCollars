871569ae0fabf1c50ee1c67053c51f581fb1a596
Radek Borowski <r.borowski@vp.pl>
2022-10-25 13:07:22 +0200

    Lesson 6 task 8 Personal ID number

---
+++ src/main/java/com/futurecollars/lesson6/task8/IllegalLengthException.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lesson6.task8;
+
+public class IllegalLengthException extends RuntimeException {
+
+  public IllegalLengthException(String message) {
+    super(message);
+  }
+}
