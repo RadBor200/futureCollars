35a4923516ad8589cc8a7540d3360a7255466d07
Radek Borowski <r.borowski@vp.pl>
2022-10-25 10:36:28 +0200

    Lesson 4 task 6 bill

--- src/main/java/com/futurecollars/lesson4/task6/BillCalculator.java
+++ src/main/java/com/futurecollars/lesson4/task6/BillCalculator.java
@@ -1,2 +1,16 @@
-package com.futurecollars.lesson4.task6;public class BillCalculator {
+package com.futurecollars.lesson4.task6;
+
+public class BillCalculator {
+
+  public static double calculate(double billValue, double serviceCharge) {
+    return billValue + serviceCharge;
+  }
+
+  public static double calculate(double billValue, double serviceCharge, double discount) {
+    return billValue + serviceCharge - discount;
+  }
+
+  public static double calculate(double billValue, double serviceCharge, int packagingFee) {
+    return billValue + serviceCharge + packagingFee;
+  }
 }
