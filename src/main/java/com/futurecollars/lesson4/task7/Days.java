86d80f915c6c1327d3616b9d20250506d894925a
Radek Borowski <r.borowski@vp.pl>
2022-10-25 10:47:09 +0200

    Lesson 4 task 7 Days of Week

---
+++ src/main/java/com/futurecollars/lesson4/task7/Days.java
@@ -0,0 +1,11 @@
+package com.futurecollars.lesson4.task7;
+
+public enum Days {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
---
+++ src/main/java/com/futurecollars/lesson4/task7/NameOfTheWeek.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lesson4.task7;
+
+public class NameOfTheWeek {
+
+  public static String printNameOfTheWeek (Days day) {
+    return String.valueOf(day);
+  }
+}
