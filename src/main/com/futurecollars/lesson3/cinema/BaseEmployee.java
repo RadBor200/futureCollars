159ac464801913f1b15c0a81266fd3bed3f713d6
Radek Borowski <r.borowski@vp.pl>
2022-10-27 13:02:28 +0200

    Lesson 3 task 3 cinema employees

---
+++ src/main/com/futurecollars/lesson3/cinema/BaseEmployee.java
@@ -0,0 +1,31 @@
+package main.com.futurecollars.lesson3.cinema;
+
+public abstract class BaseEmployee {
+
+  private String name;
+  private String surname;
+  private double salary;
+  private final int yearOfHire;
+
+  public BaseEmployee(String name, String surname, int yearOfHire) {
+    this.name = name;
+    this.surname = surname;
+    this.yearOfHire = yearOfHire;
+    this.salary = calculateMonthlySalary();
+  }
+
+  public abstract double calculateMonthlySalary();
+
+  public abstract double getBaseSalary();
+
+  public int getYearsWorked(int currentYear) {
+    return currentYear - yearOfHire;
+  }
+
+  public String toString() {
+    return String.format(
+        "Name: %s, Surname: %s, Year of hire: %s, Salary: %s",
+        name, surname, yearOfHire, calculateMonthlySalary()
+    );
+  }
+}
