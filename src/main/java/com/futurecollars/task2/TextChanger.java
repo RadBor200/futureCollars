b91b2bbfeb46d2ec6a0405ad4589debce04f2570
Radek Borowski <r.borowski@vp.pl>
2022-10-27 11:21:18 +0200

    Lesson 7 task 2 big letters

---
+++ src/main/java/com/futurecollars/task2/TextChanger.java
@@ -0,0 +1,11 @@
+package com.futurecollars.task2;
+
+public class TextChanger {
+
+  public static String changeTextToUpperCase(String text) {
+    if (text == null) {
+      return "";
+    }
+    return text.toUpperCase();
+  }
+}
