13b2c2c639799c65cd6dc6e22d0a4e5fa4f54bff
Radek Borowski <r.borowski@vp.pl>
2022-11-03 12:24:59 +0100

    Lesson 9 task 1 Own Map

---
+++ src/main/java/com/futurecollars/Lesson9/OwnMap.java
@@ -0,0 +1,22 @@
+package com.futurecollars.Lesson9;
+
+public interface OwnMap<K, V> {
+
+  boolean put(K key, V value);
+  boolean containsKey(K key);
+  boolean containsValue(V value);
+
+  /**
+   * @param key
+   *  key for which (key, value) entry will be removed
+   *
+   * @return value for given key
+   */
+  V remove(K key);
+
+  /**
+   * Return value for given key
+   */
+  V get(K key);
+
+}
