0194aa689fe4c0922ab614223bedca1e5d678cfd
Radek Borowski <r.borowski@vp.pl>
2022-11-07 11:09:20 +0100

    Lesson 9 task 5

---
+++ src/main/java/com/futurecollars/lesson9/task5/BubbleApp.java
@@ -0,0 +1,13 @@
+package com.futurecollars.lesson9.task5;
+
+import java.util.Arrays;
+
+public class BubbleApp {
+  public static void main(String[] args) {
+    Integer[] Ininteger = new Integer[]{7,2,3,5,1,8,9};
+    SortBubble<Integer> sortBubble = new SortBubble<>(Ininteger);
+    Integer[] outInteger = sortBubble.bubbleSort();
+
+    System.out.println(Arrays.toString(outInteger));
+  }
+}
