92dfad17164b3b078734b7a10fb09d9db85312cb
Radek Borowski <r.borowski@vp.pl>
2022-11-11 10:29:13 +0100

    Lesson 11 task 5 corrected

--- src/main/java/com/futurecollars/lesson11/task5/Main.java
+++ src/main/java/com/futurecollars/lesson11/task5/Main.java
@@ -1,32 +1,13 @@
 package com.futurecollars.lesson11.task5;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
+
   public static void main(String[] args) {
-
-    List<String> animals = new ArrayList<>(Arrays.asList("Cat", "Dog", "Monkey", "Elephant",
-        "Giraffe", "Hippopotamus", "Donkey", "Fish"));
-
-    List<String> namesToUpperCase = animals
-        .stream()
-        .map(String::toUpperCase)
-        .map(Main::removeDuplicates)
-        .collect(Collectors.toList());
-
-    namesToUpperCase.forEach(System.out::println);
+    List<String> words = new ArrayList<>(List.of("Cat,Dog,Monkey,Elephant,Giraffe,Hippopotamus,Donkey,Fish"));
+    WordDisplayer.displayWords(words);
   }
 
-  public static String removeDuplicates(String input) {
-    StringBuilder stringBuilder = new StringBuilder();
-
-    input
-        .chars()
-        .distinct()
-        .forEach(c -> stringBuilder.append((char) c));
-    return stringBuilder.toString();
-  }
 }
