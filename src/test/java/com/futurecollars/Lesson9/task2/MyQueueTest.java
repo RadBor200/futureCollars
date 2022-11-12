4d2bf807860ad52ec19d7f2bb546cd905f6388c5
Radek Borowski <r.borowski@vp.pl>
2022-11-12 11:26:42 +0100

    Lesson 9 task 2 corrected

--- src/test/java/com/futurecollars/Lesson9/task2/MyQueueTest.java
+++ src/test/java/com/futurecollars/Lesson9/task2/MyQueueTest.java
@@ -1,87 +1,65 @@
 package com.futurecollars.Lesson9.task2;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.NoSuchElementException;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MyQueueTest {
-  MyQueue myQueue = new MyQueue();
-  MyQueue myQueue2 = new MyQueue();
 
-  @BeforeEach
-  void setMyQueue() {
-    myQueue.add(0);
-    myQueue.add(1);
-    myQueue.add(2);
-    myQueue.add(3);
-    myQueue.add(4);
-    myQueue.add(5);
-    myQueue.add(6);
-    myQueue.add(7);
+  MyQueue MyQueue = new MyQueue();
+
+  @ParameterizedTest(name = "Element: {0}")
+  @ValueSource(ints = {123, 124, 125})
+  void shouldBeAdd(Integer element) {
+    //when
+    boolean expectAdded = MyQueue.add(element);
+    //then
+    assertTrue(expectAdded);
   }
 
   @Test
-  void shouldVerifyAdd() {
-
-    boolean isAdded = myQueue.add(8);
-    int size = myQueue.size();
-
-    Assertions.assertTrue(isAdded);
-    Assertions.assertEquals(9, size);
+  void shouldBeRemoveFirstElement() {
+    //given
+    MyQueue.add(11);
+    MyQueue.add(12);
+    MyQueue.add(7);
+    //when
+    Integer expectFirstElement = MyQueue.remove();
+    //then
+    Assertions.assertEquals(11, expectFirstElement);
   }
 
   @Test
-  @DisplayName("Should verify removing element from queue")
-  void shouldVerifyRemove() {
-
-    myQueue.remove();
-    Integer newHead = myQueue.element();
-
-    Assertions.assertEquals(7, myQueue.size());
-    Assertions.assertEquals(1, newHead);
+  void shouldNotRemoveElementFromEmptyList() {
+    //given
+    //when
+    Integer expectFirstElement = MyQueue.remove();
+    //then
+    assertNull(expectFirstElement);
   }
 
   @Test
-  @DisplayName("Should throw NoSuchElementException when trying to remove elements from empty queue")
-  void shouldThrowNoSuchElementException1() {
-
-    try {
-      myQueue2.remove();
-      Assertions.fail("This method should throw NoSuchElementException");
-    } catch (NoSuchElementException e) {
-      String message = e.getMessage();
-      boolean rightMessage = message.equals("Can't remove, queue is empty");
-      Assertions.assertTrue(rightMessage);
-    }
+  void shouldBeFirstElement() {
+    //given
+    MyQueue.add(211);
+    MyQueue.add(312);
+    MyQueue.add(447);
+    //when
+    Integer expectFirstElement = MyQueue.element();
+    //then
+    Assertions.assertEquals(211, expectFirstElement);
   }
 
   @Test
-  @DisplayName("Should verify retrieving head from queue, without removing")
-  void shouldVerifyRetrieveAndNotRemove() {
-
-    int sizeBefore = myQueue.size();
-    Integer head = myQueue.element();
-    int sizeAfter = myQueue.size();
-
-    Assertions.assertEquals(sizeBefore, sizeAfter);
-    Assertions.assertEquals(0, head);
+  void shouldNotCheckFirstElementFromEmptyList() {
+    //given
+    //when
+    Integer expectFirstElement = MyQueue.element();
+    //then
+    assertNull(expectFirstElement);
   }
 
-  @Test
-  @DisplayName("Should throw NoSuchElementException when trying to retrieve elements from empty queue")
-  void shouldThrowNoSuchElementException2() {
-
-    try {
-      myQueue2.element();
-      Assertions.fail("This method should throw NoSuchElementException");
-
-    } catch (NoSuchElementException e) {
-      String message = e.getMessage();
-      boolean rightMessage = message.equals("Can't retrieve, queue is empty");
-      Assertions.assertTrue(rightMessage);
-    }
-  }
 }
