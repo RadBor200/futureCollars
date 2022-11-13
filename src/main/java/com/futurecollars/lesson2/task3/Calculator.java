1d980ecc4396821dfd4987d80def8da0a747f037
Radek Borowski <r.borowski@vp.pl>
2022-11-13 13:08:38 +0100

    Lesson 2 task 3 corrected

---
+++ src/main/java/com/futurecollars/lesson2/task3/Calculator.java
@@ -0,0 +1,21 @@
+package com.futurecollars.lesson2.task3;
+
+public class Calculator {
+
+  public static int add(int number1, int number2) {
+    return number1 + number2;
+  }
+
+  public static int subtract(int number1, int number2) {
+    return number1 - number2;
+  }
+
+  public static int multiply(int number1, int number2) {
+    return number1 * number2;
+  }
+
+  public static double divide(double number1, double number2) {
+    return number1 / number2;
+
+  }
+}
--- src/main/java/com/futurecollars/lesson4/task7/WeekDay.java
+++
@@ -1,8 +0,0 @@
-package com.futurecollars.lesson4.task7;
-
-public class WeekDay {
-
-  public static String WhichWeekDay (Days day) {
-    return String.valueOf(day);
-  }
-}
